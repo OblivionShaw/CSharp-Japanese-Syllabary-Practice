a4fd8a9038cd9dd798b77f4d575c4ccda82b00fb
OblivionShaw <sh960208@gmail.com>
2023-12-01 23:35:53 +0800

    feat：Dictionary

---
+++ JapaneseSyllabaryPractice/JapaneseSyllabaryPractice/JapaneseSyllabary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JapaneseSyllabaryPractice
+{
+    internal class JapaneseSyllabary
+    {
+        Dictionary<string, string> Hiragana = new Dictionary<string, string>()
+        {
+            {"あ", "a"},
+        };
+
+        Dictionary<string, string> Katakana = new Dictionary<string, string>()
+        {
+            {"ア", "a"},
+        };
+    }
+}
